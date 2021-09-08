2f626bfeb078afe9f08344739ef2446e7f5bb44b
Louis Lemoine <louislemoine92@gmail.com>
2021-09-08 17:08:19 +0200

    Update index.js

--- slices/stats/index.js
+++ slices/stats/index.js
@@ -1,4 +1,14 @@
+import Image from "next/image";
 import { RichText } from "prismic-reactjs";
+
+function fromPrismic(url) {
+    const path = url.match(/\/[^\/]+$/g);
+    return `${path[0]}`;
+  }
+  
+  const myLoader = ({ src, quality = 75, width }) => {
+    return `https://images.prismic.io/prismic-shows/${src}?auto=compress&q=${quality}&w=${width}`;
+  };
 
 export default function Stats({
     items,
@@ -9,11 +19,28 @@ export default function Stats({
         <section className="bg-gray-50">
             <div className="container mx-auto py-8 max-w-7xl">
                 <h2 className="font-semibold text-center text-3xl">{RichText.asText(primary.section_title)}</h2>
-                <div className="grid grid-cols-4 items-center gap-12">
+                <div className="grid grid-cols-4 items-center gap-6 pt-12">
                     {
                         items.map((id)=> {
                             return (
-                                <p>{id.amount}</p>
+                                <div className="bg-white p-6 rounded shadow-sm">
+                                    <div className="flex">
+                                        <Image
+                                            quality={99}
+                                            alt={id.icon.alt}
+                                            loader={myLoader}
+                                            layout="intrinsic"
+                                            height={id.icon.dimensions.height}
+                                            width={id.icon.dimensions.width}
+                                            src={fromPrismic(id.icon.url)}
+                                        />
+                                        {console.log(id.icon.dimensions.height)}
+                                        <div className="ml-4">
+                                            <p className="text-gray-400">{id.stat_name}</p>
+                                            <p className="font-bold text-2xl">{id.unit + id.amount}</p>
+                                        </div>
+                                    </div>
+                                </div>
                             )
                         })
                     }
