80a6c5be2e4c104f9205dd64a59b030c3858406b
Benjamin <benjammartin@gmail.com>
2021-09-08 17:08:55 +0200

    ok

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,12 +1,15 @@
 import { Client, linkResolver } from "@lib/prismic";
 const preview = async (req, res) => {
   const redirectingTo = await Client(req)
-    .getPreviewResolver(req?.query?.token, req?.query?.documentId)
+    .getPreviewResolver(
+      req?.query?.token || null,
+      req?.query?.documentId || null
+    )
     .resolve(linkResolver, "/");
   if (!redirectingTo) {
     return res.status(401).json({ message: "Invalid token" });
   }
-  res.setPreviewData({ ref: req?.query?.token });
+  res.setPreviewData({ ref: req?.query?.token || null });
   res.write(
     `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${redirectingTo}" />
       <script>window.location.href = '${redirectingTo}'</script>
